2d2b6b14edabb58cfde7a5346c0c611c0f898d1b
Deepinnacle <deepinnacleofficial@gmail.com>
2023-02-13 03:44:36 -0800

    added transition for the navbar

--- app/comps/Navbar.tsx
+++ app/comps/Navbar.tsx
@@ -2,12 +2,12 @@ import Link from "next/link"
 const Navbar = () => {
     return (
         <nav className={`w-full h-screen p-2 box-border bg-zinc-800 text-right`}>
-            <Link href="/" className="text-white block p-6 text-xl font-extralight hover:bg-zinc-900">Home</Link>
-            <Link href="/about" className="text-white block p-6 text-xl font-extralight hover:bg-zinc-900">About</Link>
-            <Link href="/resume" className="text-white block p-6 text-xl font-extralight hover:bg-zinc-900">Resume</Link>
-            <Link href="/services" className="text-white block p-6 text-xl font-extralight hover:bg-zinc-900">Services</Link>
-            <Link href="/portfolio" className="text-white block p-6 text-xl font-extralight hover:bg-zinc-900">Portfolio</Link>
-            <Link href="/contact" className="text-white block p-6 text-xl font-extralight hover:bg-zinc-900">Contact</Link>
+            <Link href="/" className="anchor">Home</Link>
+            <Link href="/about" className="anchor">About</Link>
+            <Link href="/resume" className="anchor">Resume</Link>
+            <Link href="/services" className="anchor">Services</Link>
+            <Link href="/portfolio" className="anchor">Portfolio</Link>
+            <Link href="/contact" className="anchor">Contact</Link>
         </nav>
     )
 }
